fbf8c9c2fc1df2202baed885d6a9a80b0b9ab23d
bermilion <volkov.i@studio2i.ru>
2026-04-14 20:41:23 +0300

    работа над базовым шрифтом

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,20 @@
     <body class="body">
 		<div class="box"></div>
 
+		<div class="viewports">
+			<p class="viewport viewport_mobile">Мобильное устройство</p>
+			<p class="viewport viewport_phone">Смартфон</p>
+			<p class="viewport viewport_phone-portrait">Смартфон вертикальный</p>
+			<p class="viewport viewport_phone-landscape">Смартфон горизонтальный</p>
+			<p class="viewport viewport_tablet">Планшет</p>
+			<p class="viewport viewport_tablet-portrait">Планшет вертикальный</p>
+			<p class="viewport viewport_tablet-landscape">Планшет горизонтальный</p>
+			<p class="viewport viewport_desktop">Десктоп</p>
+			<p class="viewport viewport_laptop">Ноутбук</p>
+			<p class="viewport viewport_wide">Широкоэкранник</p>
+			<p class="viewport viewport_no-phone">Все кроме телефонов</p>
+		</div>
+
 		<div class="wrapper">
 			<div class="grid">
 				<div class="grid__item"></div>
@@ -25,8 +39,15 @@
 				<div class="grid__item"></div>
 			</div>
 		</div>
+		<div class="info"></div>
 
-		<p class="hero">Hero text</p>
+		<div class="wrapper">
+			<p class="hero_px">Достопримечательности</p>
+			<p class="hero">Достопримечательности</p>
+			<p class="supheading">Таблица с ценами на модели минипланет</p>
+			<h1 class="h1">Достопримечательности</h1>
+			<p class="p">Первый раз минипланеты были упомянут французским лётчиком в книге для взрослых детей. Тогда читатели углубились в философский аспект произведения, не придав значения идее частных планет-астероидов. Но с середины прошлого века технологии развились достаточно, чтобы предложить людям особенную замену классичискому решению жилищного вопроса.</p>
+		</div>
 
 		<div class="debug-grid"></div>
     </body>
