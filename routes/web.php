226550e1741544c514c11ff472b28fb7101eb3f3
Bermilion <bermilion@studiobrain.ru>
2026-04-15 16:54:42 +0300

    Добавил страницы

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/pages/{view}', function ($view) {
+    return view("pages.{$view}");
+})->where('view', '.*');
