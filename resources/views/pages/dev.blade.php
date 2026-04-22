17f431638c6f32bb4d865e83a5ad88a138342532
Bermilion <bermilion@studiobrain.ru>
2026-04-22 12:06:12 +0300

    начал работу над цветовой новой схемой

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -1,2 +1,9 @@
 <x-layouts.base-layout title="Страница отладки">
+
+	<div class="grid">
+		@foreach (["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"] as $color)
+			<div class="box box_red-{{ $color }}"></div>
+		@endforeach
+	</div>
+
 </x-layouts.base-layout>
