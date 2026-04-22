476428975a677879a02ff2e663b4fc32eb37ee3f
Bermilion <bermilion@studiobrain.ru>
2026-04-22 16:43:27 +0300

    Работа над функцией генерации оттенков

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -1,11 +1,19 @@
+@php
+	$colors = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"];
+@endphp
+
 <x-layouts.base-layout title="Страница отладки">
 
 	<div class="grid">
-		@foreach (["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"] as $color)
+		@foreach ($colors as $color)
 			<div class="box box_red-{{ $color }}"></div>
 		@endforeach
+		@foreach ($colors as $color)
+			<div class="box box_accent-{{ $color }}"></div>
+		@endforeach
+		@foreach ($colors as $color)
+			<div class="box box_gray-{{ $color }}"></div>
+		@endforeach
 	</div>
-
-	<div class="pfpf">Text</div>
 
 </x-layouts.base-layout>
