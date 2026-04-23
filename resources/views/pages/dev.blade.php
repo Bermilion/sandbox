76b6a8648e760a1ada1c42c65524525a4e4ef559
Bermilion <bermilion@studiobrain.ru>
2026-04-23 12:38:06 +0300

    Отладка функций работы с цветом

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -1,12 +1,15 @@
 @php
-	$colors = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"];
+	$steps = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"];
+	$substeps = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"];
 @endphp
 
 <x-layouts.base-layout title="Страница отладки">
 
 	<div class="grid">
-{{--		@foreach ($colors as $color)--}}
-{{--			<div class="box box_red-{{ $color }}"></div>--}}
+{{--		@foreach ($steps as $step)--}}
+{{--			@foreach($substeps as $substep)--}}
+{{--				<div class="box box_red-{{ $step }}-{{ $substep }}"></div>--}}
+{{--			@endforeach--}}
 {{--		@endforeach--}}
 {{--		@foreach ($colors as $color)--}}
 {{--			<div class="box box_accent-{{ $color }}"></div>--}}
@@ -14,7 +17,9 @@
 {{--		@foreach ($colors as $color)--}}
 {{--			<div class="box box_gray-{{ $color }}"></div>--}}
 {{--		@endforeach--}}
-		<div class="box box_color"></div>
+{{--		<div class="box box_color"></div>--}}
+		<div class="box box_1"></div>
+		<div class="box box_2"></div>
 	</div>
 
 </x-layouts.base-layout>
