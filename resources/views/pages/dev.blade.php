2f28c9408a6ab7b23fb9129eddbcea4b1b6a8681
Bermilion <bermilion@studiobrain.ru>
2026-04-24 17:00:29 +0300

    Отладка функций работы с цветом 4

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -11,13 +11,18 @@
 {{--				<div class="box box_red-{{ $step }}-{{ $substep }}"></div>--}}
 {{--			@endforeach--}}
 {{--		@endforeach--}}
-{{--		@foreach ($colors as $color)--}}
-{{--			<div class="box box_accent-{{ $color }}"></div>--}}
+{{--		@foreach ($steps as $step)--}}
+{{--			<div class="box box_color-{{ $step }}">{{ $step }}</div>--}}
 {{--		@endforeach--}}
-		<div class="box box_red-origin-50"></div>
-		@foreach ($steps as $step)
-			<div class="box box_red-my-{{ $step }}"></div>
-		@endforeach
+		<div class="box box_color-1"></div>
+		<div class="box box_color-2"></div>
+		<div class="box box_color-3"></div>
+		<div class="box box_color-4"></div>
+		<div class="box box_color-5"></div>
+{{--		<div class="box box_red-origin-50"></div>--}}
+{{--		@foreach ($steps as $step)--}}
+{{--			<div class="box box_red-my-{{ $step }}"></div>--}}
+{{--		@endforeach--}}
 {{--		<div class="box box_color"></div>--}}
 {{--		<div class="box box_1"></div>--}}
 {{--		<div class="box box_2"></div>--}}
