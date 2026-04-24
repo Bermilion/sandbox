edcbcefc92637caabdd02b5a668b480a78822b56
bermilion <volkov.i@studio2i.ru>
2026-04-24 18:12:43 +0300

    Проверил работоспособность функций

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -14,11 +14,17 @@
 {{--		@foreach ($steps as $step)--}}
 {{--			<div class="box box_color-{{ $step }}">{{ $step }}</div>--}}
 {{--		@endforeach--}}
-		<div class="box box_color-1"></div>
-		<div class="box box_color-2"></div>
-		<div class="box box_color-3"></div>
-		<div class="box box_color-4"></div>
-		<div class="box box_color-5"></div>
+		<div class="box box_color-6">light-50 50</div>
+		<div class="box box_color-5">light-40 100</div>
+		<div class="box box_color-4">light-30 200</div>
+		<div class="box box_color-3">light-20 300</div>
+		<div class="box box_color-2">light-10 400</div>
+		<div class="box box_color-1">base 500</div>
+		<div class="box box_color-7">dark-10 600</div>
+		<div class="box box_color-8">dark-20 700</div>
+		<div class="box box_color-9">dark-30 800</div>
+		<div class="box box_color-10">dark-40 900</div>
+		<div class="box box_color-11">dark-50 950</div>
 {{--		<div class="box box_red-origin-50"></div>--}}
 {{--		@foreach ($steps as $step)--}}
 {{--			<div class="box box_red-my-{{ $step }}"></div>--}}
