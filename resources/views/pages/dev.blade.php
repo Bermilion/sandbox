13386c18661a2e9c9d531e529d37999fbaa4425c
Bermilion <bermilion@studiobrain.ru>
2026-04-27 16:11:57 +0300

    Тестирование функций работы с цветом

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -5,33 +5,32 @@
 
 <x-layouts.base-layout title="Страница отладки">
 
+	<x-type::h size="5">OKLCH:</x-type::h>
+	<x-type::hint>С подстройкой интерполяции через tailwind</x-type::hint>
 	<div class="grid">
-{{--		@foreach ($steps as $step)--}}
-{{--			@foreach($substeps as $substep)--}}
-{{--				<div class="box box_red-{{ $step }}-{{ $substep }}"></div>--}}
-{{--			@endforeach--}}
-{{--		@endforeach--}}
-{{--		@foreach ($steps as $step)--}}
-{{--			<div class="box box_color-{{ $step }}">{{ $step }}</div>--}}
-{{--		@endforeach--}}
-		<div class="box box_color-6">accent base</div>
-		<div class="box box_color-5">light-10 400</div>
-		<div class="box box_color-4">dark-10 600</div>
-		<div class="box box_color-3">hex dark-10 600</div>
-		<div class="box box_color-2">hex light-10 400</div>
-		<div class="box box_color-1">base 500</div>
-		<div class="box box_color-7">dark-10 600</div>
-		<div class="box box_color-8">dark-20 700</div>
-		<div class="box box_color-9">dark-30 800</div>
-		<div class="box box_color-10">dark-40 900</div>
-		<div class="box box_color-11">dark-50 950</div>
-{{--		<div class="box box_red-origin-50"></div>--}}
-{{--		@foreach ($steps as $step)--}}
-{{--			<div class="box box_red-my-{{ $step }}"></div>--}}
-{{--		@endforeach--}}
-{{--		<div class="box box_color"></div>--}}
-{{--		<div class="box box_1"></div>--}}
-{{--		<div class="box box_2"></div>--}}
+		@foreach($steps as $step)
+			<div class="box box_{{ $step }}">
+				{{ $step }}
+			</div>
+		@endforeach
+	</div>
+
+	<x-type::h size="5">OKLCH:</x-type::h>
+	<div class="grid">
+		@foreach($steps as $step)
+			<div class="box box_oklch-{{ $step }}">
+				{{ $step }}
+			</div>
+		@endforeach
+	</div>
+
+	<x-type::h size="5">HSL:</x-type::h>
+	<div class="grid">
+		@foreach($steps as $step)
+			<div class="box box_hsl-{{ $step }}">
+				{{ $step }}
+			</div>
+		@endforeach
 	</div>
 
 </x-layouts.base-layout>
