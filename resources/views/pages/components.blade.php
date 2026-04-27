a2742a5538ed9dbcc7c4ee7530db5f25751d201c
Bermilion <bermilion@studiobrain.ru>
2026-04-27 16:12:18 +0300

    Тестирование компонента link

--- resources/views/pages/components.blade.php
+++ resources/views/pages/components.blade.php
@@ -1,7 +1,8 @@
 <x-layouts.base-layout title="Компоненты">
 	<x-layouts.documentation>
 		<x-layouts.wrapper class="my-64">
-			<x-base::link href="#" text="Ссылка" />
+			<x-base::link href="#" text="Ссылка" icon="thumbs-up" />
+
 		</x-layouts.wrapper>
 	</x-layouts.documentation>
 </x-layouts.base-layout>
