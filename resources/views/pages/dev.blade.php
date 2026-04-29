3f5906157dd40f13144376ebe52347830e4f3223
Bermilion <bermilion@studiobrain.ru>
2026-04-29 11:56:00 +0300

    Отладка функций работы с размерами и отступами

--- resources/views/pages/dev.blade.php
+++ resources/views/pages/dev.blade.php
@@ -1,3 +1,13 @@
-<x-layouts.base-layout title="Страница отладки">
+@php
+	$scales = [1, 2, 4, 8, 12, 16, 20, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 240, 320, 800];
+@endphp
 
+<x-layouts.base-layout title="Страница отладки">
+	<div class="block-scales">
+		@foreach($scales as $scale)
+			<div class="box-scale box-scale_{{ $scale }}"></div>
+		@endforeach
+
+		<div class="box-scale box-scale_dark-blue"></div>
+	</div>
 </x-layouts.base-layout>
